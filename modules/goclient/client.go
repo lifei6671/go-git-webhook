493602e891c8ce326ef2bc61dc07f3182b8b7737
Minho <longfei6671@163.com>
2017-03-23 18:06:45 +0800

    测试Client模式连接

--- modules/goclient/client.go
+++ modules/goclient/client.go
@@ -1,18 +1,89 @@
 package goclient
 
-import "time"
+import (
+	"github.com/gorilla/websocket"
+	"net/http"
+	"errors"
+	"strings"
+	"io/ioutil"
+	"github.com/widuu/gojson"
+	"strconv"
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+)
 
-type ClientConfig struct {
-	User string
-	Password string
-	Timeout time.Duration
+type WebHookClient struct {
+	token string
+	conn *websocket.Conn
+}
+func Connection(remoteUrl  string,token string) (*WebHookClient ,error){
+	client :=  &WebHookClient{
+
+	}
+
+	header := http.Header{}
+	header.Add("x-smarthook-token",token)
+	c, _, err := websocket.DefaultDialer.Dial(remoteUrl,header)
+
+	if err != nil {
+		return client,err
+	}
+	client.conn = c
+	return client,nil
 }
 
-type Client struct {
+func (c *WebHookClient)Send(msg []byte) error {
 
+	return c.conn.WriteMessage(websocket.TextMessage, msg)
 }
 
-func Dial(addr string,port int,config *ClientConfig) (*Client,error){
+func (c *WebHookClient) Read() ([]byte,error) {
+	_, message, err := c.conn.ReadMessage()
 
-	return &Client{},nil
+	return message,err
+}
+
+func (c *WebHookClient) Close() {
+	c.conn.Close()
+}
+func GetToken(remoteUrl,account string,password string) (string,error) {
+
+
+	t := strconv.Itoa(time.Now().Nanosecond())
+	hash := sha256.New()
+	hash.Write([]byte(account + password + t))
+	md := hash.Sum(nil)
+	mdStr := hex.EncodeToString(md)
+
+	response, err := http.Post(remoteUrl,
+		"application/x-www-form-urlencoded",
+		strings.NewReader("account=" + account +"&password=" + mdStr + "&time=" + t))
+	if err != nil {
+		return "",err
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "",err
+	}
+
+	if js := gojson.Json(string(body)).Get("error_code");js.IsValid() {
+		error_code,err := strconv.Atoi(js.Tostring())
+
+		if err != nil {
+			return "",err
+		}
+		if error_code != 0 {
+			message := gojson.Json(string(body)).Get("message");
+
+			return "",errors.New(message.Tostring())
+		}
+		token := gojson.Json(string(body)).Get("data")
+
+		return token.Tostring(),nil
+
+	}
+	return "",errors.New("Data error")
+
 }
