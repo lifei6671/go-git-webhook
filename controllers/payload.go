29eaaea94417ffc7733046aaf44d00a993c07c29
sam <yimingsha@qq.com>
2018-04-26 10:01:00 +0800

    修复关联多服务器一直处于等待执行的Bug

--- controllers/payload.go
+++ controllers/payload.go
@@ -1,69 +1,69 @@
 package controllers
 
 import (
-	"github.com/lifei6671/go-git-webhook/models"
-	"github.com/astaxie/beego/logs"
-	"github.com/lifei6671/go-git-webhook/tasks"
-	"github.com/lifei6671/go-git-webhook/modules/hooks"
-	"strings"
 	"errors"
+	"github.com/astaxie/beego/logs"
+	"github.com/lifei6671/go-git-webhook/models"
+	"github.com/lifei6671/go-git-webhook/modules/hooks"
+	"github.com/lifei6671/go-git-webhook/tasks"
+	"strings"
 )
 
 // PayloadController Git回调页面控制器.
 type PayloadController struct {
 	BaseController
 }
+
 // Index 回调首页 .
 func (c *PayloadController) Index() {
 	c.Prepare()
 	c.TplName = ""
 
 	key := c.Ctx.Input.Param(":key")
-	if key == ""{
+	if key == "" {
 		c.Abort("404")
 	}
 
 	webHook := models.NewWebHook()
 
-	if err := webHook.FindByKey(key);err != nil {
+	if err := webHook.FindByKey(key); err != nil {
 		c.Ctx.WriteString("Git WebHook no found.")
 		c.StopRun()
 	}
 
 	body := string(c.Ctx.Input.RequestBody[0:])
 
-
-	if body == ""{
+	if body == "" {
 		c.Ctx.WriteString("Request body error.")
 		c.StopRun()
 	}
 
-	hookData ,err := createWebHook(webHook.HookType,body)
+	hookData, err := createWebHook(webHook.HookType, body)
 
 	if err != nil {
 		c.Ctx.WriteString(err.Error())
-		logs.Error("",err.Error())
+		logs.Error("", err.Error())
 		c.StopRun()
 	}
 
-	branch,err := hookData.BranchName()
+	branch, err := hookData.BranchName()
 
 	if err != nil {
 		c.Ctx.WriteString("Request is not valid Git webhook: " + err.Error())
-		logs.Error("Get Branch Name => ",err)
+		logs.Error("Get Branch Name => ", err)
 		c.StopRun()
 	}
 
-	repo ,err := hookData.RepositoryName()
+	repo, err := hookData.RepositoryName()
 
 	if err != nil {
 		c.Ctx.WriteString("Request is not valid Git webhook: " + err.Error())
-		logs.Error("Get Repository Name => ",err.Error())
+		logs.Error("Get Repository Name => ", err.Error())
 		c.StopRun()
 	}
 
-	if webHook.RepositoryName != repo || (webHook.BranchName != branch && "heads/"+ webHook.BranchName != branch){
-		c.Ctx.WriteString( "Not match the Repo and Branch.")
+	if webHook.RepositoryName != repo || (webHook.BranchName != branch && "heads/"+webHook.BranchName != branch) {
+		c.Ctx.WriteString("Not match the Repo and Branch.")
 		logs.Error("Not match the Repo and Branch.")
 		c.StopRun()
 	}
@@ -73,22 +73,22 @@ func (c *PayloadController) Index() {
 	scheduler.WebHookId = webHook.WebHookId
 	scheduler.Data = body
 
-	if push,err := hookData.UserName() ;err == nil {
+	if push, err := hookData.UserName(); err == nil {
 		scheduler.PushUser = push
 	}
-	if value,err := hookData.AfterValue(); err == nil{
+	if value, err := hookData.AfterValue(); err == nil {
 		scheduler.ShaValue = value
-	}else{
-		logs.Error("",err.Error())
+	} else {
+		logs.Error("", err.Error())
 		scheduler.ShaValue = "无"
 	}
 
 	scheduler.Status = "wait"
 
-	relationDetailed,err := models.FindRelationDetailedByWhere("AND relation.web_hook_id = ?",webHook.WebHookId)
+	relationDetailed, err := models.FindRelationDetailedByWhere("AND relation.web_hook_id = ?", webHook.WebHookId)
 
 	if err != nil {
-		logs.Error(5001,err.Error())
+		logs.Error(5001, err.Error())
 
 		c.Ctx.WriteString("Data error")
 		c.StopRun()
@@ -98,10 +98,9 @@ func (c *PayloadController) Index() {
 		c.StopRun()
 	}
 
+	schedulerList := make([]models.Scheduler, len(relationDetailed))
 
-	schedulerList := make([]models.Scheduler,len(relationDetailed))
-
-	for i,relation := range relationDetailed {
+	for i, relation := range relationDetailed {
 
 		scheduler.SchedulerId = 0
 		scheduler.ServerId = relation.ServerId
@@ -111,34 +110,34 @@ func (c *PayloadController) Index() {
 		schedulerList[i] = *scheduler
 	}
 
+	schedulerId := models.NewScheduler().GetMaxSchedulerId()
+	nums, err := models.NewScheduler().InsertMulti(schedulerList)
 
-	nums,err := models.NewScheduler().InsertMulti(schedulerList)
-
-	if nums <=0 || err != nil {
+	if nums <= 0 || err != nil {
 		if err != nil {
-			logs.Error(0,err.Error())
+			logs.Error(0, err.Error())
 
 		}
 		c.Ctx.WriteString("Data error")
 	}
 	for _, scheduler := range schedulerList {
-		tasks.Add(tasks.Task{ SchedulerId : scheduler.SchedulerId ,ServerId:scheduler.ServerId,WebHookId:scheduler.WebHookId})
+		schedulerId = schedulerId + 1
+		tasks.Add(tasks.Task{SchedulerId: schedulerId, ServerId: scheduler.ServerId, WebHookId: scheduler.WebHookId})
 	}
 
 	c.Ctx.WriteString("Work put into Queue.")
 	c.StopRun()
 }
 
-func createWebHook(t string,data string) (hooks.WebHookRequestInterface,error) {
-	if strings.EqualFold(t,"github") {
+func createWebHook(t string, data string) (hooks.WebHookRequestInterface, error) {
+	if strings.EqualFold(t, "github") {
 		return hooks.NewGitHubWebHook(data)
-	}else if strings.EqualFold(t,"gitlab") {
+	} else if strings.EqualFold(t, "gitlab") {
 		return hooks.NewGitLabWebHook(data)
-	}else if strings.EqualFold(t,"gogs") {
+	} else if strings.EqualFold(t, "gogs") {
 		return hooks.NewGogsWebHook(data)
-	}else if strings.EqualFold(t,"gitosc") {
+	} else if strings.EqualFold(t, "gitosc") {
 		return hooks.NewGitOSCWebHook(data)
 	}
-	return nil,errors.New("The type does not support.")
+	return nil, errors.New("The type does not support.")
 }
-
